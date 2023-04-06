2cb7c5393a788a7dc71c91c2764912298dfc20c4
Nadja Djordan <nadjadj1908@gmail.com>
2023-04-06 10:12:27 +0200

    inicijalno projekat

---
+++ domaci/src/test/java/com/domaci/domaci/DomaciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domaci.domaci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomaciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
